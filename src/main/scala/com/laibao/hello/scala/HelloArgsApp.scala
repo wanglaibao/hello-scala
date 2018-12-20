9dcd183c6c41f9b1589914f084bb126c0d57eea4
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-20 23:38:38 +0800

    update README.md file info

--- src/main/scala/com/laibao/hello/scala/HelloArgsApp.scala
+++ src/main/scala/com/laibao/hello/scala/HelloArgsApp.scala
@@ -1,7 +1,20 @@
 package com.laibao.hello.scala
 
 object HelloArgsApp extends App{
-
   if (args.size == 0) println("Hello, World") else println("Hello, " + args(0))
-
+  println()
+  val name = "John Doe"
+  println()
+  println("hello".head)
+  println()
+  println("hello".tail)
+  println()
+  println("hello, world".take(5))
+  println()
+  println("hi")
+  println(1 + 2 * 3)
+  println()
+  println((1 + 2) * 3)
+  println()
+  println(if (2 > 1) println("greater") else println("lesser"))
 }
