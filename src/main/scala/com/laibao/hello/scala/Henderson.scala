c64bb01ab833d939a98d943cbd695db491d986dd
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-01-16 21:47:01 +0800

    update README.md file info

---
+++ src/main/scala/com/laibao/hello/scala/Henderson.scala
@@ -0,0 +1,8 @@
+package com.laibao.hello.scala
+
+object Henderson {
+
+  val name:String = "Henderson"
+  val color:String = "Ginger"
+  val food:String = "Chips"
+}
---
+++ src/main/scala/com/laibao/hello/scala/Quentin.scala
@@ -0,0 +1,8 @@
+package com.laibao.hello.scala
+
+object Quentin {
+
+  val name:String = "Quentin"
+  val color:String ="Tabby and white"
+  val food:String = "Curry"
+}
