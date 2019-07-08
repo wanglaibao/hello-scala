c82608ba7fbcd3bc3ae941c76cebecd28ea8547f
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-07-08 10:34:47 +0800

    update README.md file info

--- src/main/scala/com/laibao/hello/scala/fromjavatoscala/Parameters.scala
+++ src/main/scala/com/laibao/hello/scala/fromjavatoscala/Parameters.scala
@@ -2,10 +2,16 @@ package com.laibao.hello.scala.fromjavatoscala
 
 object Parameters extends App{
     def max(values: Int*) = values.foldLeft(values(0)) { Math.max }
+
+    def min(values:Int*):Int = values.foldLeft(values(0)){Math.min}
+
     println(max(8, 2, 3))
     println(max(2, 5, 3, 7, 1, 6))
 
     function(1, 2, 3)
 
     def function(input: Int*): Unit = println(input.getClass)
+
+    val numbers = Array(2, 5, 3, 7, 1, 6)
+    function(numbers: _*)
 }
